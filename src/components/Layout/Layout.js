a560cc61e364fe4d6c0b1ceaa37aaa91033605d9
buiminhhai1 <ngovietduc20088@gmail.com>
2019-08-07 11:10:01 +0700

    add fix layout for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React,{Component} from 'react';
 import classes from './Layout.module.css';
 import Aux from '../../hoc/Auxinliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) =>(
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>{props.children}</main>
-    </Aux>
-);
+class Layout extends Component{
+    state ={
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () =>{
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>{this.props.children}</main>
+            </Aux>);
+    }
+} 
 export default Layout;
