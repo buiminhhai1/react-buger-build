3e81bd2350acf8d6c9bbd74e0d78389756ca4205
buiminhhai1 <ngovietduc20088@gmail.com>
2019-08-19 03:31:39 +0700

    fix use updateObject in Auth containers

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -83,12 +83,11 @@ class Auth extends Component {
 
 
         const updatedControls = updateObject(this.state.controls, {
-            [controlName] : {
-                ...this.state.controls[controlName],
+            [controlName] : updateObject(this.state.controls[controlName],{
                 value: event.target.value,
                 valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
-            }
+            })
         });
         this.setState({controls: updatedControls});
     }
