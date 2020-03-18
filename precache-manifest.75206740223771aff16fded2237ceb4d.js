2f009fdb95eeb7b6598cac4c74731c81851cc427
Bùi Minh Hải <39934622+buiminhhai1@users.noreply.github.com>
2020-03-18 14:59:41 +0700

    Updates

---
+++ precache-manifest.75206740223771aff16fded2237ceb4d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94da1a896234f854d0c4eb8038c088c2",
+    "url": "/react-buger-build/index.html"
+  },
+  {
+    "revision": "6b9fbfbc100800a12d76",
+    "url": "/react-buger-build/static/css/3.e933fb1d.chunk.css"
+  },
+  {
+    "revision": "e122a52d080e8c706591",
+    "url": "/react-buger-build/static/css/4.26df5a61.chunk.css"
+  },
+  {
+    "revision": "5316cc5010759ac74e6a",
+    "url": "/react-buger-build/static/css/5.ba53f5aa.chunk.css"
+  },
+  {
+    "revision": "ddebf0c3e685f25fe580",
+    "url": "/react-buger-build/static/css/main.0fbf7eba.chunk.css"
+  },
+  {
+    "revision": "dfabc00804067b29d583",
+    "url": "/react-buger-build/static/js/2.1738fabe.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-buger-build/static/js/2.1738fabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9fbfbc100800a12d76",
+    "url": "/react-buger-build/static/js/3.7fbb8ae3.chunk.js"
+  },
+  {
+    "revision": "e122a52d080e8c706591",
+    "url": "/react-buger-build/static/js/4.6cb4fe79.chunk.js"
+  },
+  {
+    "revision": "5316cc5010759ac74e6a",
+    "url": "/react-buger-build/static/js/5.a955096b.chunk.js"
+  },
+  {
+    "revision": "ddebf0c3e685f25fe580",
+    "url": "/react-buger-build/static/js/main.b455f675.chunk.js"
+  },
+  {
+    "revision": "94b389a7c1adfe406fe1",
+    "url": "/react-buger-build/static/js/runtime-main.708e268c.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-buger-build/static/media/burger-logo.b8503d26.png"
+  }
+]);
